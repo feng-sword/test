ab2011c098637af1ed28309f5d1324b15b697a4c
fenghao0000000 <17611620626@163.com>
2017-12-03 13:49:22 +0800

    commit

---
+++ src/main/java/Test1.java
@@ -0,0 +1,16 @@
+public class Test1 {
+    public static int test(){
+        try{
+            return 1;
+        }catch (Exception e){
+            return 2;
+        }finally {
+            return 3;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(test());
+
+    }
+}
